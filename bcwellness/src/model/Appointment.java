df45d297fa23f47bfe28d197a59cea268acc7b42
slmey <p3l00ts@gmail.com>
2025-07-16 13:07:27 +0200

    appointment

--- bcwellness/src/model/Appointment.java
+++ bcwellness/src/model/Appointment.java
@@ -1,4 +1,19 @@
 package model;
 
 public class Appointment {
+    private int id;
+    private String studentName;
+    private int counselorId;
+    private String date;
+    private String time;
+    private String status;
+
+    public Appointment(int id, String studentName, int counselorId, String date, String time, String status) {
+        this.id = id;
+        this.studentName = studentName;
+        this.counselorId = counselorId;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
 }
