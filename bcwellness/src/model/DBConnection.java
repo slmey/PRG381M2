35a61ae6df54550f58d02063dd84a669ec54c81c
slmey <p3l00ts@gmail.com>
2025-07-16 13:05:05 +0200

    dbconnections

---
+++ bcwellness/src/model/DBConnection.java
@@ -0,0 +1,15 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:derby://localhost:1527/db; create=true";
+    private static final String USER = "app";
+    private static final String PASSWORD = "app";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
