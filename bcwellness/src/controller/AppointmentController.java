2ab6c5c0c848950385b801817f487aae61a80c3a
slmey <p3l00ts@gmail.com>
2025-07-16 13:37:04 +0200

    Update AppointmentController.java

--- bcwellness/src/controller/AppointmentController.java
+++ bcwellness/src/controller/AppointmentController.java
@@ -12,10 +12,10 @@ public class AppointmentController {
         try (Connection conn = DBConnection.getConnection()){
             String sql = "INSERT INTO appointments (id,studentName,counselorId,date,time,status) VALUES (?,?,?,?,?,?)";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setInt(1, appointment.getId);
-            ps.setString(2, appointment.getstudentName());
+            ps.setInt(1, appointment.getId());
+            ps.setString(2, appointment.getStudentName());
             ps.setInt(3, appointment.getCounselorId());
-            ps.setDate(4, appointment.getDarte());
+            ps.setDate(4, appointment.getDate());
             ps.setTime(5, appointment.getTime());
             ps.setString(6, appointment.getStatus());
             ps.executeUpdate();
@@ -23,25 +23,27 @@ public class AppointmentController {
             e.printStackTrace();
         }
     }
+
+    public static ArrayList<Appointment> getAllAppointments(){
+        ArrayList<Appointment> list = new ArrayList<>();
+        try (Connection conn = DBConnection.getConnection()){
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Appointments");
+            while (rs.next()){
+                list.add(new Appointment(
+                        rs.getInt("id"),
+                        rs.getString("studentName"),
+                        rs.getInt("counselorId"),
+                        rs.getDate("date").toString(),
+                        rs.getTime("time").toString(),
+                        rs.getString("status")
+                ));
+            }
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
 
-public static ArrayList<Appointment> getAllAppointments(){
-    ArrayList<Appointment> list = new ArrayList<>();
-    try (Connection conn = DBConnection.getConnection()){
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("SELECT * FROM Appointments");
-        while (rs.next()){
-            list.add(new Appointment(
-                    rs.getInt("id"),
-                    rs.getString("studentName"),
-                    rs.getInt("counselorId"),
-                    rs.getDate("date").toString(),
-                    rs.getTime("time").toString(),
-                    rs.getString("status")
-            ));
-        }
-    }catch (SQLException e) {
-        e.printStackTrace();
-    }
-    return list;
-}
+
